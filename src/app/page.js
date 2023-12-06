84e066a83a81567b3b031b13f904dead115ad90d
templeajuzie <templeajuzie@gmail.com>
2023-12-06 15:38:06 +0100

    setting up home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,34 @@
-import Image from 'next/image'
+'use client'; // This is a client component 👈🏽
+import Image from 'next/image';
+import Dashboard from './Components/Dashboard/Dashboard';
+import PostHome from './Components/Post/PostHome';
+import Announcement from './Components/Announcement/Announcement';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { CircularProgress } from '@mui/material';
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userauth.user);
+  const isAuthenticated = useSelector(
+    (state) => state.userauth.isAuthenticated
+  );
+  const loading = useSelector((state) => state.userauth.loading);
+  const error = useSelector((state) => state.userauth.error);
+
+  useEffect(() => {
+    dispatch({ type: 'userauth/getUserInformation' });
+  }, [dispatch]);
+  
+
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
-  )
+    <main className=''>
+      <Navbar />
+      <Dashboard />
+      <PostHome />
+      <Footer />
+    </main>
+  );
 }
