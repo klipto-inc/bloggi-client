77ae8aab4df1662e319392d8e45a1440a8aba665
templeajuzie <templeajuzie@gmail.com>
2023-12-07 16:58:34 +0100

    feat: adding new files

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,12 @@
-'use client'; // This is a client component 👈🏽
-import Image from 'next/image';
-import Dashboard from './Components/Dashboard/Dashboard';
-import PostHome from './Components/Post/PostHome';
-import Announcement from './Components/Announcement/Announcement';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { CircularProgress } from '@mui/material';
+'use client'; 
+
 
 export default function Home() {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.userauth.user);
-  const isAuthenticated = useSelector(
-    (state) => state.userauth.isAuthenticated
-  );
-  const loading = useSelector((state) => state.userauth.loading);
-  const error = useSelector((state) => state.userauth.error);
-
-  useEffect(() => {
-    dispatch({ type: 'userauth/getUserInformation' });
-  }, [dispatch]);
   
 
   return (
     <main className=''>
-      <Navbar />
-      <Dashboard />
-      <PostHome />
-      <Footer />
+      <h1>Hello</h1>
     </main>
   );
 }
