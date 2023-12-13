9fceae44e9e78f1950f0c8bacefe0b27e8e5f1fe
templeajuzie <templeajuzie@gmail.com>
2023-12-13 19:29:28 +0100

    fix: bottom navigate

--- src/app/Components/BottomNavigation/BottomNav.js
+++ src/app/Components/BottomNavigation/BottomNav.js
@@ -1,39 +1,31 @@
 import React from "react";
+import Link from "next/link";
+import { IoCreateOutline } from "react-icons/io5";
+import { MdOutlineExplore } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const BottomNav = () => {
   return (
-    <div className="md:hidden">
-      <section class="block fixed bottom-0 inset-x-0 z-50 shadow-lg text-gray-800 bg-gray-300 dark:bg-dark backdrop-blur-lg bg-opacity-30 dark:bg-opacity-20 dark:text-gray-400 border-t-2 border-royal/20">
-        <div id="tabs" class="flex justify-between">
-          <a
-            href="#"
-            class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1 hover:bg-white"
-            activeClass="dark:text-gray-100 text-black"
+    <div classNameName="md:hidden">
+      <section className="fixed inset-x-0 bottom-0 z-50 block text-gray-800 bg-gray-300 border-t-2 shadow-lg dark:bg-dark backdrop-blur-lg bg-opacity-30 dark:bg-opacity-20 dark:text-gray-400 border-royal/20">
+        <div id="tabs" className="flex justify-between">
+          <Link
+            href="/"
+            className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-royal hover:text-royal hover:bg-white"
+            activeClassName="dark:text-gray-100 text-black border-1 border-t-[#FF3131]"
+          >
+            
+            <MdOutlineExplore className="inline-block w-6 h-6 mb-1" />
+            <span className="block text-xs tab">Feed</span>
+          </Link>
+          <Link
+            href="/category"
+            className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-royal hover:text-royal hover:bg-white"
+            activeClassName="dark:text-gray-100 text-black border-1 border-t-[#FF3131]"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 inline-block mb-1"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-              />
-            </svg>
-            <span class="tab block text-xs">Home</span>
-          </a>
-          <a
-            href="#"
-            class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1 hover:bg-white"
-            activeClass="dark:text-gray-100 text-black"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 inline-block mb-1"
+              className="inline-block w-6 h-6 mb-1"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -45,37 +37,25 @@ const BottomNav = () => {
                 d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
               />
             </svg>
-            <span class="tab block text-xs">Categories</span>
-          </a>
-          <a
-            href="#"
-            class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1 hover:bg-white"
-            activeClass="dark:text-gray-100 text-black"
+            <span className="block text-xs tab">Categories</span>
+          </Link>
+          <Link
+            href="/createpost"
+            className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-royal hover:text-royal hover:bg-white"
+            activeClassName="dark:text-gray-100 text-black"
+          >
+            
+            <IoCreateOutline className="inline-block w-6 h-6 mb-1"/>
+            <span className="block text-xs tab">Create</span>
+          </Link>
+          <Link
+            href="user/profile"
+            className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-royal hover:text-royal hover:bg-white"
+            activeClassName="dark:text-gray-100 text-black border-1 border-t-[#FF3131]"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 inline-block mb-1"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-            <span class="tab block text-xs">Gallery</span>
-          </a>
-          <a
-            href="#"
-            class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1 hover:bg-white"
-            activeClass="dark:text-gray-100 text-black"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 inline-block mb-1"
+              className="inline-block w-6 h-6 mb-1"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -87,8 +67,18 @@ const BottomNav = () => {
                 d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
               />
             </svg>
-            <span class="tab block text-xs">About</span>
-          </a>
+            <span className="block text-xs tab">Profile</span>
+          </Link>
+          <Link
+            href="/settings"
+            className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-royal hover:text-royal hover:bg-white"
+            activeClassName="dark:text-gray-100 text-black border-1 border-t-[#FF3131]"
+          >
+            
+            <IoSettingsOutline className="inline-block w-6 h-6 mb-1" />
+            
+            <span className="block text-xs tab">Settings</span>
+          </Link>
         </div>
       </section>
     </div>
