7ccb2db3a5e81aa14888e7f35f1535a3c6114c26
templeajuzie <templeajuzie@gmail.com>
2023-12-21 11:11:04 +0100

    fix bottom nav

--- src/Components/BottomNavigation/BottomNav.js
+++ src/Components/BottomNavigation/BottomNav.js
@@ -85,6 +85,30 @@ const BottomNav = () => {
           </Link>
         )}
 
+        {!user && (
+          <Link
+            href="/signin"
+            className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-royal hover:text-royal hover:bg-white text-[#000]" 
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="inline-block w-6 h-6 mb-1"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+              />
+            </svg>
+
+            <span className="block text-xs text-black tab">Profile</span>
+          </Link>
+        )}
+
         <Link
           href="/settings"
           className={`justify-center inline-block w-full pt-2 pb-1 text-center focus:text-royal hover:text-royal hover:bg-white ${
