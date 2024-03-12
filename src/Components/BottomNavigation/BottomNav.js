944bcc78bb55efe2622da5fc1d90517b1f6790d7
Apple <apple@Apples-MacBook-Pro.local>
2024-03-12 01:37:53 +0100

    fix: mobile auth

--- src/Components/BottomNavigation/BottomNav.js
+++ src/Components/BottomNavigation/BottomNav.js
@@ -50,7 +50,7 @@ const BottomNav = () => {
         <Link
           href={user ? "/dashboard/create" : "/auth/signin"}
           className={`justify-center inline-block w-full pt-2 pb-1 text-center focus:text-royal hover:text-royal hover:bg-white ${
-            pathName === "/create" ? "text-[#FF3131] bg-white" : "text-[#000]"
+            pathName === "/dashboard/create" ? "text-[#FF3131] bg-white" : "text-[#000]"
           }`}
         >
           <IoCreateOutline className="inline-block w-6 h-6 mb-1" />
